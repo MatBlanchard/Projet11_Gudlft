cd49f7cd12c586a6854bfd24094b7b4bdf7f79f4
matbl <matblanchard.pro@gmail.com>
2023-02-16 18:01:01 +0100

    app 100% fonctionnelle

--- tests/unit_tests/test_index.py
+++
@@ -1,7 +0,0 @@
-class TestIndex:
-
-    def test_index_route_should_return_index_page(self, client, captured_templates):
-        response = client.get('/')
-        assert response.status_code == 200
-        template, context = captured_templates[0]
-        assert template.name == 'index.html'
